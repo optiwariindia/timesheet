4300d4b0bcb00ed6b0803bea1543403c00c2dba1
Om Tiwari <optiwari@ivaid.org>
2019-01-12 19:43:46 +0530

    8PM 12JAN

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,11 +23,19 @@ export class HomePage {
     loginid:"",
     cnfpass:""
   };
+  form={
+    pwchange:false,
+    heading:"",
+    blockUser:false,
+  };
+  target:any;
   constructor(public navCtrl: NavController,public sidebar:ProvideSidebarProvider,public webapi:WebapiProvider) {
+    this.target={};
   }
   errorReset(){
     this.frmError.loginid="";
     this.frmError.cnfpass="";
+    
   }
   formReset(){
     this.newUser={
@@ -62,6 +70,77 @@ export class HomePage {
         }
       });
     }
+  }
+  changePasswd(){
+    this.newUser.sesskey=this.sidebar.userinfo.sesskey;
+    this.errorReset();
+    if(this.newUser.passwd!=this.newUser.cnfpass){
+      this.frmError.cnfpass="Your password is not matching";
+    }else{
+      var apiresp=this.webapi.getData(this.newUser);
+      apiresp.subscribe(r=>{
+        var resp=JSON.parse(JSON.stringify(r));
+        if("status" in resp){
+          if(resp.status.type=="error"){
+
+          }else{
+            this.formReset();
+            this.hideForms();
+            this.sidebar.AdminUserList(0);
+          }
+        }
+      });
+    }
+  }
+  changePasswdForm(i){
+    this.form.heading="Change password of "+i.loginid;
+    this.form.pwchange=true;
+    this.newUser={
+      loginid:i.loginid,
+      name:i.name,
+      designation:i.designation,
+      passwd:"",
+      cnfpass:"",
+      module:"users",
+      action:"changePasswd",
+      sesskey:""
+    }
+  }
+  hideForms(){
+    this.form={
+      pwchange:false,
+      heading:"",
+      blockUser:false
+    }
+  }
+  changeDetails(i){
+    console.log(i);
+  }
+  blockUserForm(i){
+    this.form.blockUser=true;
+    this.form.heading="Do you really want to block "+i.loginid;
+    this.target=i;
+  }
+  activateUserForm(i){
+    this.form.blockUser=true;
+    this.form.heading="Do you want to activate " + i.loginid;
+    this.target=i;
+  }
+  blockUser(i){
+    var usr={};
+    if(i.active==1){
+      usr={loginid:i.loginid,module:"users",action:"block",sesskey:this.sidebar.userinfo.sesskey};
+    }else{
+      usr={loginid:i.loginid,module:"users",action:"active",sesskey:this.sidebar.userinfo.sesskey};
+    }
+    var r=this.webapi.getData(usr);
+    r.subscribe(resp=>{
+      var rsp=JSON.parse(JSON.stringify(resp));
+      if(rsp.status.result){
+        this.hideForms();
+        this.sidebar.AdminUserList(0);
+      }
+    });
   }
 
 }
